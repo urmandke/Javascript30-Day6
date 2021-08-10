3f4b482c8354cc8e8733abb8889dfbae7bd9049e
Udayraj <udayraj.m@bizamica.com>
2021-08-10 08:22:22 +0530

    Feat: function FindMatches() implementation complete

--- script.js
+++ script.js
@@ -6,5 +6,11 @@ fetch(endpoint)
     .then(blob => blob.json())
     .then(data => cities.push(...data));
 
-
+function findMatches(wordToMatch, cities){
+    return cities.filter(place => {
+        //here we need to figure out if the city or state matches what was searched
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex)
+    });
+}
 
